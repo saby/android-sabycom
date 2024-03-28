4b384ec3db5045cef376ddbdec11bf2e6de05be9
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-03-28 18:00:28 +0300

    new change

---
+++ sabycom/src/main/java/ru/tensor/sabycom/push/PushNotificationCenter.kt
@@ -0,0 +1,88 @@
+package ru.tensor.sabycom.push
+
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import ru.tensor.sabycom.push.builder.MessagePushNotificationBuilder
+import ru.tensor.sabycom.push.builder.PushNotificationBuilder
+import ru.tensor.sabycom.push.lifecycle.AppLifecycleTracker
+import ru.tensor.sabycom.push.lifecycle.SabycomLifecycleTracker
+import ru.tensor.sabycom.push.manager.NotificationManager
+import ru.tensor.sabycom.push.manager.NotificationManagerProvider
+import ru.tensor.sabycom.push.manager.PushNotificationManager
+import ru.tensor.sabycom.push.manager.ToastNotificationManager
+import ru.tensor.sabycom.push.parser.PushNotificationParser
+import ru.tensor.sabycom.push.parser.SabycomPushNotificationParser
+import ru.tensor.sabycom.push.parser.data.PushType
+import ru.tensor.sabycom.widget.repository.RemoteRepository
+import ru.tensor.sabycom.push.util.NotificationChannelUtil
+
+/**
+ * @author am.boldinov
+ */
+internal class PushNotificationCenter(
+    private val context: Context,
+    private val repository: RemoteRepository,
+    private val lifecycleTracker: AppLifecycleTracker,
+    private val parser: PushNotificationParser
+) : SabycomPushService {
+
+    constructor(
+        context: Context,
+        repository: RemoteRepository
+    ) : this(
+        context,
+        repository,
+        SabycomLifecycleTracker(context),
+        SabycomPushNotificationParser()
+    )
+
+    private val managerProvider = NotificationManagerHolder()
+    private val pushBuilderMap = mutableMapOf<PushType, PushNotificationBuilder>()
+    private val handler = Handler(Looper.getMainLooper())
+
+    init {
+        initNotificationChannels()
+        initPushBuilders()
+    }
+
+    override fun isSabycomPushNotification(payload: Map<String, String>): Boolean {
+        return parser.isValidPayload(payload)
+    }
+
+    override fun handlePushNotification(payload: Map<String, String>) {
+        val message = parser.parse(payload)
+        pushBuilderMap[message.type]?.build(message)?.let { notification ->
+            handler.post {
+                managerProvider.get().notify("TAG", 0, notification.build()) // TODO tag & id
+            }
+        }
+    }
+
+    override fun sendToken(token: String) {
+        repository.sendPushToken(token)
+    }
+
+    private fun initNotificationChannels() {
+        NotificationChannelUtil.submitDefaultNotificationChannel(context, "САБИДОК") // TODO name
+    }
+
+    private fun initPushBuilders() {
+        pushBuilderMap[PushType.MESSAGE] = MessagePushNotificationBuilder(context)
+    }
+
+    private inner class NotificationManagerHolder : NotificationManagerProvider {
+
+        private val pushManager by lazy { PushNotificationManager(context) }
+        private val toastManager by lazy { ToastNotificationManager(context) }
+
+        override fun get(): NotificationManager {
+            return if (lifecycleTracker.isAppInForegroundNow()) {
+                toastManager
+            } else {
+                pushManager
+            }
+        }
+
+    }
+}
