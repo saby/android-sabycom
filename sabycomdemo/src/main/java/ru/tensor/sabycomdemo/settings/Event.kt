64ad344de8e669c616eb0ea28d4778f878137628
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-18 18:01:00 +0300

    new change

---
+++ sabycomdemo/src/main/java/ru/tensor/sabycomdemo/settings/Event.kt
@@ -0,0 +1,21 @@
+package ru.tensor.sabycomdemo.settings
+
+/**
+ * Событие для LiveData которое обрабатывается один раз.
+ *
+ * @author ma.kolpakov
+ */
+internal class Event<out T>(private val content: T) {
+
+    private var isHandled = false
+
+    /**
+     * Получить контент. Если событие еще не было обработано
+     */
+    fun handleEvent(handler: (T) -> Unit) {
+        if (!isHandled) {
+            isHandled = true
+            handler(content)
+        }
+    }
+}
