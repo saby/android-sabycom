ef39cd08356dbf045b885b9515aee582e7043f73
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-21 18:01:06 +0300

    new change

---
+++ sabycom/src/main/java/ru/tensor/sabycom/widget/repository/LocalRepository.kt
@@ -0,0 +1,15 @@
+package ru.tensor.sabycom.widget.repository
+
+import ru.tensor.sabycom.data.UserData
+
+/**
+ * @author ma.kolpakov
+ */
+internal interface LocalRepository {
+    fun savePushToken(token: String)
+    fun getPushToken(): String?
+    fun saveUser(user: UserData)
+    fun saveApiKey(apiKey: String)
+    fun getApiKey(): String?
+    fun getUserData(): UserData?
+}
