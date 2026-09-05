764025fd15a71b52a012ea46901d67da39e6f901
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-05 18:03:30 +0300

    new change

---
+++ sabycom/src/main/java/ru/tensor/sabycom/push/builder/NotificationData.kt
@@ -0,0 +1,23 @@
+package ru.tensor.sabycom.push.builder
+
+import ru.tensor.sabycom.push.parser.data.PushCloudAction
+
+/**
+ * Базовый интерфейс для модели данных уведомления, описывающий обязательные свойства.
+ * @property tag тэг уведомления, под которым оно будет опубликовано
+ * @property id идентификатор уведомления
+ * @property chatUUID идентификатор чата
+ * @property title заголовок уведомления
+ * @property text текст уведомления
+ * @property action действие с уведомлением
+ *
+ * @author am.boldinov
+ */
+internal interface NotificationData {
+    val tag: String
+    val id: Int
+    val chatUUID: String
+    val title: String
+    val text: String
+    val action: PushCloudAction
+}
