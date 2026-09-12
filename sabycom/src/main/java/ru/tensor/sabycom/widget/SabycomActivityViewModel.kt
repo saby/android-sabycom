92110562d249c748f618efb56a6796bbd8541d4b
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-12 18:04:11 +0300

    new change

---
+++ sabycom/src/main/java/ru/tensor/sabycom/widget/SabycomActivityViewModel.kt
@@ -0,0 +1,84 @@
+package ru.tensor.sabycom.widget
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.tensor.sabycom.Sabycom
+import ru.tensor.sabycom.data.UrlUtil
+import ru.tensor.sabycom.data.UserData
+import ru.tensor.sabycom.widget.network.NetworkAvailable
+import ru.tensor.sabycom.widget.network.NetworkAvailableImpl
+import ru.tensor.sabycom.widget.repository.Repository
+import ru.tensor.sabycom.data.Event
+
+/**
+ * @author ma.kolpakov
+ */
+internal class SabycomActivityViewModel(
+    application: Application,
+    private val repository: Repository = Sabycom.repository,
+    private val networkAvailable: NetworkAvailable = NetworkAvailableImpl()
+) :
+    AndroidViewModel(application), NetworkAvailable by networkAvailable {
+
+    constructor(application: Application) : this(application, Sabycom.repository)
+
+    private val _openEvent = MutableLiveData<OpenWidgetData>()
+    val openEvent: LiveData<OpenWidgetData> = _openEvent
+
+    private val _closeEvent = MutableLiveData<Unit>()
+    val closeEvent: LiveData<Unit> = _closeEvent
+
+    private val _pageReady = MutableLiveData<Unit>()
+    val pageReady: LiveData<Unit> = _pageReady
+
+    private val _openChatEvent = MutableLiveData<Event<String>>()
+    /** Событие открытия чата. */
+    val openChatEvent: LiveData<Event<String>> = _openChatEvent
+
+    init {
+        _openEvent.value = OpenWidgetData(
+            UrlUtil.buildWidgetUrl(
+                userId = repository.requireUserData().id.toString(),
+                apiKey = repository.requireApiKey()
+            ),
+            repository.requireUserData(),
+            repository.requireApiKey()
+        )
+        Sabycom.sabycomFeature?.onClose = {
+            hide()
+        }
+        networkAvailable.bind(application)
+    }
+
+    override fun onCleared() {
+        Sabycom.sabycomFeature?.onClose = null
+        networkAvailable.unbind()
+    }
+
+    fun hide() {
+        _closeEvent.postValue(Unit)
+    }
+
+    fun showWebView() {
+        _pageReady.postValue(Unit)
+    }
+
+    /**
+     * Выполняется в момент готовности страницы.
+     */
+    fun onPageStarted() {
+        repository.getChatIdAndForget()?.let {
+            _openChatEvent.postValue(Event(it))
+        }
+    }
+
+    fun showWebViewOnline() {
+        if (isNetworkAvailable()) {
+            _pageReady.postValue(Unit)
+        }
+    }
+
+    internal data class OpenWidgetData(val url: String, val userData: UserData, val channel: String)
+}
