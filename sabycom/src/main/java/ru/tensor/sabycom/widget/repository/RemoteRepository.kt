5231f61ac1c9f88600fea34a04a2d45067381070
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-18 18:02:33 +0300

    new change

---
+++ sabycom/src/main/java/ru/tensor/sabycom/widget/repository/RemoteRepository.kt
@@ -0,0 +1,20 @@
+package ru.tensor.sabycom.widget.repository
+
+import ru.tensor.sabycom.data.UserData
+
+/**
+ * @author am.boldinov
+ */
+internal interface RemoteRepository {
+
+    fun performRegisterSync(
+        apiKey: String,
+        userData: UserData,
+        token: String?,
+        serviceType: String?,
+        isUnsubscribe: Boolean = false
+    )
+
+    fun getUnreadMessageCount(apiKey: String, userData: UserData, callback: (Int) -> Unit)
+
+}
